af7bed2771c259a5d3ed49fa4c497d3879abd861
Jairo Lopez <jrlopezsolar@gmail.com>
2013-03-29 17:34:42 -0500

    Add unit test to Watir/Selenium checker
    Use RSpec to wrap the execution of Watir

---
+++ test/rspec_test_watir_browser.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require 'watir-webdriver'
+ 
+describe "An Amazon search" do
+  it "should display results range" do
+  end
+ 
+  it "should reflect the search context in the title" do
+  end
+end
