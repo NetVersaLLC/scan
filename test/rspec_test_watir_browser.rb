933d0f18d2b11c27b2134db13323c9193439822a
Jairo Lopez <jrlopezsolar@gmail.com>
2013-03-29 17:42:27 -0500

    Update test over test_watir_browser
    Added before and after methods.

--- test/rspec_test_watir_browser.rb
+++ test/rspec_test_watir_browser.rb
@@ -2,23 +2,26 @@ require 'rspec'
 require 'watir-webdriver'
  
 describe "An Amazon search" do
-  it "should display results range" do
+  before :each do
     @browser = Watir::Browser.new :firefox
     @browser.goto("http://www.amazon.com")
+  end
+ 
+  it "should display results range" do
     @browser.select_list(:id, "searchDropdownBox").select("Books")
     @browser.text_field(:name, 'field-keywords').set("star wars")
     @browser.button(:alt, "Go").click
     @browser.text.should match(/Showing .* of .* Results/)
-    @browser.close
   end
  
   it "should reflect the search context in the title" do
-    @browser = Watir::Browser.new :firefox
-    @browser.goto("http://www.amazon.com")
     @browser.select_list(:id, "searchDropdownBox").select("Books")
     @browser.text_field(:name, 'field-keywords').set("star wars")
     @browser.button(:alt, "Go").click
     @browser.title.should == "Amazon.com: star wars: Books"
+  end
+ 
+  after :each do
     @browser.close
   end
 end
