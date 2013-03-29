ec76406b06863d1d7204d5274325f8cebc142f6f
Jairo Lopez <jrlopezsolar@gmail.com>
2013-03-29 17:39:17 -0500

    Implement test over test_watir_browser.rb
    This outputs the name of each behavior (“Amazon search”) and each
    example of that behavior that I want to check.

--- test/rspec_test_watir_browser.rb
+++ test/rspec_test_watir_browser.rb
@@ -3,8 +3,22 @@ require 'watir-webdriver'
  
 describe "An Amazon search" do
   it "should display results range" do
+    @browser = Watir::Browser.new :firefox
+    @browser.goto("http://www.amazon.com")
+    @browser.select_list(:id, "searchDropdownBox").select("Books")
+    @browser.text_field(:name, 'field-keywords').set("star wars")
+    @browser.button(:alt, "Go").click
+    @browser.text.should match(/Showing .* of .* Results/)
+    @browser.close
   end
  
   it "should reflect the search context in the title" do
+    @browser = Watir::Browser.new :firefox
+    @browser.goto("http://www.amazon.com")
+    @browser.select_list(:id, "searchDropdownBox").select("Books")
+    @browser.text_field(:name, 'field-keywords').set("star wars")
+    @browser.button(:alt, "Go").click
+    @browser.title.should == "Amazon.com: star wars: Books"
+    @browser.close
   end
 end
