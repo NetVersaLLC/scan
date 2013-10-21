09a199d78b7c063235bdc5932d49c69b97e31a55
nixoid <andrey.zamovskiy@skykillers.com>
2013-10-20 20:15:36 -0700

    udpated staging server address

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
-set :branch, 'master'
+set :branch, 'production'
 set :rails_env, :staging
-set :domain, ['ec2-50-17-135-19.compute-1.amazonaws.com'] # replace this
+set :domain, ['ec2-54-234-206-170.compute-1.amazonaws.com']
 ssh_options[:forward_agent] = true
 
 role :web, *domain
